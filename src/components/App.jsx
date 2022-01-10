8d53473675a4843fbb090e1e4483c3915baa7fdb
hebercruz384 <73669492+hebercruz384@users.noreply.github.com>
2022-01-10 18:28:02 -0500

    !

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,8 @@ import "../css/App.css";
 import data from "../sample_data.json";
 
 function App() {
-  return <div className="app">Trivia!</div>;
+  return <div className="app">Trivia!!</div>;
 }
 
 export default App;
+//----
