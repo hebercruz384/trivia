defe96252ec0cad8c1d81596acbea7f417273001
hebercruz384 <73669492+hebercruz384@users.noreply.github.com>
2022-03-14 18:36:47 -0400

    Used props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,11 +4,17 @@ import data from "../sample_data.json";
 
 function App() {
   console.log(data);
-  return <div className="app">Trivia!!</div>;
+  let number = 1;
+  return (
+    <div className="app">
+      Triviaaa!!
+      <Question text={Question.text} />
+    </div>
+  );
 }
-function Question(props){
-  
+function Question(props) {
+  console.log(props);
+  return <div>{props.text}</div>;
 }
-
 export default App;
 //----
