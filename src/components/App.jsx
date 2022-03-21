f4ee7070c2beab1733e5e5cafb7945c5aa3a6344
hebercruz384 <73669492+hebercruz384@users.noreply.github.com>
2022-03-21 17:03:20 -0400

    Finished Day 1 Goals.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,17 +4,21 @@ import data from "../sample_data.json";
 
 function App() {
   console.log(data);
-  let number = 1;
+  let number = 0;
   return (
     <div className="app">
       Triviaaa!!
-      <Question text={Question.text} />
+      <Question text={data[number].question.text} />
+      <NextQuestion />
     </div>
   );
 }
 function Question(props) {
   console.log(props);
   return <div>{props.text}</div>;
+}
+function NextQuestion() {
+  return <button type="button">Next Question</button>;
 }
 export default App;
 //----
